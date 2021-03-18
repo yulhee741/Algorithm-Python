24826389f988ddf2a6b26ff3e665121d34b8275a
YulHee Kim <dbfgml980413@gmail.com>
2021-03-18 19:24:26 +0900

    Solve python 11726

--- 다이나믹 프로그래밍/11726.py
+++ 다이나믹 프로그래밍/11726.py
@@ -1,10 +1,15 @@
 import sys
 
 N = int(sys.stdin.readline())
-dp = [0, 1, 2] 
-mod = 10007 
+dp = [0 for _ in range(N+1)]
 
-for i in range(3, N + 1): 
-    dp.append((dp[i - 1] + dp[i - 2])%mod) 
-sys.stdout.write(str(dp[N]))
+if N <= 3:
+    print(N)
+else:
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[i] % 10007)
 
