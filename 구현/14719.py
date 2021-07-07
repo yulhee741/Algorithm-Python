62f1a81107e6a24423de46137b2b1279e1249f9c
YulHee Kim <dbfgml980413@gmail.com>
2021-07-07 14:55:40 +0900

    solve python

--- 구현/14719.py
+++ 구현/14719.py
@@ -1,14 +1,19 @@
 #빗물
+from sys import stdin
 
-def trapping_rain(buildings):
-    raindrop = 0
+H, W = map(int, stdin.readline().split())
+buildings = list(map(int, stdin.readline().split()))
+
+def solution(buildings):
+    rain = 0
     for i in range(len(buildings)):
         # 현재 인덱스의 왼쪽에서 가장 높은 건물의 높이        
         max_left = max(buildings[:i+1])
         # 현재 인덱스의 오른쪽에서 가장 높은 건물의 높이 
         max_right = max(buildings[i:])
-        # 둘중에 어떤 값이 더 큰가?
+        # 둘중에 어떤 값이 더 작은지 확인
         which_low = min(max_left, max_right)
-        # 절대값 주의
-        raindrop = raindrop + abs(buildings[i] - which_low)
-    return raindrop
+        rain = rain + abs(buildings[i] - which_low)
+    return rain
+
+print(solution(buildings))
