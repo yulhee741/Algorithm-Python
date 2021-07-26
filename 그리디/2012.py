5e49549bb8ccdf4fd4dc1b3b9e14c718c60c10a1
YulHee Kim <dbfgml980413@gmail.com>
2021-07-26 13:28:30 +0900

    solve python

---
+++ 그리디/2012.py
@@ -0,0 +1,17 @@
+# 등수 매기기
+from sys import stdin
+
+N = int(stdin.readline())
+score = []
+for _ in range(N):
+    score.append(int(stdin.readline()))
+
+score.sort() # 정렬
+result = 0
+for i in range(N):
+    if score[i] != i+1: # 리스트에 해당 번째 인덱스가 등수와 다를경우 
+        result += abs(score[i]-(i+1)) # 차이를 구하여 결과에 저장
+        score[i] = i+1
+
+print(result)
+
