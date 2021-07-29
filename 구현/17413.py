0c30ea29e8e481c5b5cf9e824c0ccc97928c5dc7
YulHee Kim <dbfgml980413@gmail.com>
2021-07-29 22:02:48 +0900

    solve python

--- 구현/17413.py
+++ 구현/17413.py
@@ -1,28 +1,28 @@
-import sys from stdin
+# 단어 뒤집기 2
+from sys import stdin
 
-
-s = stdin.readline.rstrip()
+s = stdin.readline().rstrip()
 flag = False
 word = ""
 answer = ""
 
-for i in s:
-    if flag == False:
-        if i == "<":
-            flag = True
-            word += i
-        elif i == " ":
-            word += i
-            answer += word
-            word = ""
-        else:
-            word = i + word
+for i in range(len(s)):
+    if s[i] == "<":
+        flag = True
+        while s[i] != ">":
+            word += s[i]
+            i += 1
+    elif s[i] == " " and flag == False:
+        word += s[i]
+        answer += word
+        word = ""
+    elif s[i] == ">" and flag == True:
+        word += s[i]
+        answer += word
+        word, flag = "", False
+    elif flag == False:
+        word = s[i] + word
 
-    else:
-        word += i
-        if i == ">":
-            flag = False
-            answer += word
-            word = ""
 
 print(answer + word)
+        
