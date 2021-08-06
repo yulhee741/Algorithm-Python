4eff90105a7c75e0421a27aded873dc18e0c41c9
YulHee Kim <dbfgml980413@gmail.com>
2021-08-06 15:39:56 +0900

    solve python

---
+++ 이코테/ch_4/4-2.py
@@ -0,0 +1,13 @@
+# 시각
+from sys import stdin
+h = int(stdin.readline())
+
+cnt = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                cnt += 1
+
+print(cnt)
