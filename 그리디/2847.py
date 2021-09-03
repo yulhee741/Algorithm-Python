676ed25d098c197b6c78c8bf9b29276466dcbcb7
yulhee741 <dbfgml980413@gmail.com>
2021-09-03 18:42:34 +0900

    solve python

--- 그리디/2847.py
+++ 그리디/2847.py
@@ -8,7 +8,7 @@ cnt = 0
 for i in range(n-1, 0, -1):
     if score[i] <= score[i-1]:
         tmp = score[i-1] - score[i] + 1
-        score[i-1] = score[i-1] - tmp
+        score[i-1] -= tmp
         cnt += tmp
 
 print(cnt)
