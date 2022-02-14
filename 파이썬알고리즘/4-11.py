2167cb87669be8cb30f51389a5f0c7ae8caecd99
yulhee741 <dbfgml980413@gmail.com>
2022-02-14 13:16:24 +0900

    solve python

---
+++ 파이썬알고리즘/4-11.py
@@ -0,0 +1,16 @@
+# 최대힙
+from sys import stdin
+import heapq as hq
+
+a = []
+while True:
+    n = int(stdin.readline())
+    if n == -1:
+        break
+    if n == 0:
+        if len(a) == 0:
+            print(-1)
+        else:
+            print(-hq.heappop(a))
+    else:
+        hq.heappush(a, -n)
